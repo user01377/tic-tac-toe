262f256a9820b3cacdaab674679a3a74eb2c5779
user01377 <144172972+user01377@users.noreply.github.com>
2024-02-06 18:03:00 -0600

    NON-FUNCTIONAL SOLUTION. created multiple functions
    the "checkVictory()" function is not working. whenever either player gets 3 in a row, function does not return correct value. not sure what other bugs there are.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ boardIndex = {
     "2": 64,
     "3": 72,
     "4": 160,
+    "5": 168,
     "6": 176,
     "7": 264,
     "8": 272,
@@ -24,9 +25,60 @@ board = '''+-------+-------+-------+
 |   7   |   8   |   9   |
 |       |       |       |
 +-------+-------+-------+'''
+
+listBoard = list(board)
+print(board)
+
 def userMove():
     userInput = input("place O where: ")
-    x = list(board)
-    x[boardIndex[userInput]] = "O"
-    newString = "".join(x)
-    print(newString)
+
+    listBoard[boardIndex[userInput]] = "O"
+
+    global updatedBoard
+    updatedBoard = "".join(listBoard)
+    print(updatedBoard)
+
+    if checkVictory("human") == "Player":
+        print("Player Wins!")
+    else:
+        computerMove()
+
+def computerMove():
+    computerInput = random.randint(1,9)
+
+    listBoard[boardIndex[str(computerInput)]] = "X"
+    updatedBoard = "".join(listBoard)
+    print(updatedBoard)
+
+    if checkVictory("computer") == "computer":
+        print("Computer Wins!")
+    else:
+        userMove()
+          
+
+def checkVictory(player):
+    currentBoard = []
+
+    #updating board to latest info
+    for position in range(1,10):
+        currentBoard.append(board[boardIndex[str(position)]])
+
+    if player == "human": 
+
+        #only 4 win conditions
+        if currentBoard[0] == "O" and currentBoard[1] == "O" and currentBoard[2] == "O" or currentBoard[6] == "O" and currentBoard[7] == "O" and currentBoard[8] == "O": 
+            return "Player"
+        elif currentBoard[0] == "O" and currentBoard[3] == "O" and currentBoard[6] == "O" or currentBoard[2] == "O" and currentBoard[5] == "O" and currentBoard[8] == "O":
+            return "Player"
+        else:
+            return "NoWin"
+
+    elif player == "computer": #must check all 8 win conditions because players first move is not a constant
+        if currentBoard[0] == "X" and currentBoard[1] == "X" and currentBoard[2] == "X" or currentBoard[3] == "X" and currentBoard[4] == "X" and currentBoard[5] == "X" or currentBoard[6] == "X" and currentBoard[7] == "X" and currentBoard[8] == "X":
+             return "computer"
+        elif currentBoard[0] == "X" and currentBoard[3] == "X" and currentBoard[6] == "X" or currentBoard[1] == "X" and currentBoard[4] == "X" and currentBoard[7] == "X" or currentBoard[2] == "X" and currentBoard[5] == "X" and currentBoard[8] == "X":
+             return "computer"
+        else:
+             return "NoWin"
+        
+userMove()
